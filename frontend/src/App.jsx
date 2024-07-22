68ae98430d4047b30ac0c5d15fc50fd1c69466f3
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-07-22 14:36:06 -0500

    create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SingIn from './pages/SingIn';
+import SingUp from './pages/SingUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 
 export default function App() {
   return (
-    <div>app</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sing-in" element={<SingIn />} />
+        <Route path="/sing-up" element={<SingUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
